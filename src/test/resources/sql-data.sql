7a95ea2eaa54d53c6a9251588270dbfee470d980
Dsoftwaredev <danielsoftwaredev@outlook.com>
2022-07-08 16:57:27 +0100

    more tests

--- src/test/resources/sql-data.sql
+++ src/test/resources/sql-data.sql
@@ -1,2 +1,3 @@
 INSERT INTO `customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
 INSERT INTO `items` (`product_name`, `product_price`) VALUES ('test', 100);
+INSERT INTO `orders` (`cust_id`, `item_id`) VALUES (1, 1);
